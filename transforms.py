6189de4334d165698c6bca6d52c602b96269ec1a
Vinh Tran <tqvinhcs@gmail.com>
2019-08-29 10:38:40 +0700

    Add files via upload

---
+++ transforms.py
@@ -0,0 +1,568 @@
+import torch
+import torchvision
+import torchvision.transforms.functional as F
+
+import math
+import random
+import numbers
+import numpy as np
+
+from PIL import Image, ImageOps
+
+
+class GroupRandomCrop(object):
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, img_group):
+
+        w, h = img_group[0].size
+        th, tw = self.size
+
+        out_images = list()
+
+        x1 = random.randint(0, w - tw)
+        y1 = random.randint(0, h - th)
+
+        for img in img_group:
+            assert(img.size[0] == w and img.size[1] == h)
+            if w == tw and h == th:
+                out_images.append(img)
+            else:
+                out_images.append(img.crop((x1, y1, x1 + tw, y1 + th)))
+
+        return out_images
+
+
+class GroupCenterCrop(object):
+    def __init__(self, size):
+        self.worker = torchvision.transforms.CenterCrop(size)
+
+    def __call__(self, img_group):
+        return [self.worker(img) for img in img_group]
+
+
+class GroupRandomHorizontalFlip(object):
+    """Randomly horizontally flips the given PIL.Image with a probability of 0.5
+    """
+    def __init__(self, is_flow=False):
+        self.is_flow = is_flow
+
+    def __call__(self, img_group, is_flow=False):
+        v = random.random()
+        if v < 0.5:
+            ret = [img.transpose(Image.FLIP_LEFT_RIGHT) for img in img_group]
+            if self.is_flow:
+                for i in range(0, len(ret), 2):
+                    ret[i] = ImageOps.invert(ret[i])  # invert flow pixel values when flipping
+            return ret
+        else:
+            return img_group
+
+
+class GroupNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        rep_mean = self.mean * (tensor.size()[0]//len(self.mean))
+        rep_std = self.std * (tensor.size()[0]//len(self.std))
+
+        # TODO: make efficient
+        for t, m, s in zip(tensor, rep_mean, rep_std):
+            t.sub_(m).div_(s)
+
+        return tensor
+
+
+class GroupResize(object):
+    """ Rescales the input PIL.Image to the given 'size'.
+    'size' will be the size of the smaller edge.
+    For example, if height > width, then image will be
+    rescaled to (size * height / width, size)
+    size: size of the smaller edge
+    interpolation: Default: PIL.Image.BILINEAR
+    """
+
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.worker = torchvision.transforms.Resize(size, interpolation)
+
+    def __call__(self, img_group):
+        return [self.worker(img) for img in img_group]
+
+
+class GroupScale(GroupResize):
+    """ Rescales the input PIL.Image to the given 'size'.
+    'size' will be the size of the smaller edge.
+    For example, if height > width, then image will be
+    rescaled to (size * height / width, size)
+    size: size of the smaller edge
+    interpolation: Default: PIL.Image.BILINEAR
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(GroupScale, self).__init__(*args, **kwargs)
+
+
+class GroupRandomResizedCrop(object):
+    """Random crop the given PIL.Image to a random size of (0.08 to 1.0) of the original size
+    and and a random aspect ratio of 3/4 to 4/3 of the original aspect ratio
+    This is popularly used to train the Inception networks
+    size: size of the smaller edge
+    interpolation: Default: PIL.Image.BILINEAR
+    """
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+        if isinstance(size, tuple):
+            self.size = size
+        else:
+            self.size = (size, size)
+        self.scale = scale
+        self.ratio = ratio
+        self.interpolation = interpolation
+
+    @staticmethod
+    def get_params(img, scale, ratio):
+        """Get parameters for ``crop`` for a random sized crop.
+        Args:
+            img (PIL Image): Image to be cropped.
+            scale (tuple): range of size of the origin size cropped
+            ratio (tuple): range of aspect ratio of the origin aspect ratio cropped
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for a random
+                sized crop.
+        """
+        area = img.size[0] * img.size[1]
+
+        for attempt in range(10):
+            target_area = random.uniform(*scale) * area
+            log_ratio = (math.log(ratio[0]), math.log(ratio[1]))
+            aspect_ratio = math.exp(random.uniform(*log_ratio))
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if w <= img.size[0] and h <= img.size[1]:
+                i = random.randint(0, img.size[1] - h)
+                j = random.randint(0, img.size[0] - w)
+                return i, j, h, w
+
+        # Fallback to central crop
+        in_ratio = img.size[0] / img.size[1]
+        if (in_ratio < min(ratio)):
+            w = img.size[0]
+            h = int(round(w / min(ratio)))
+        elif (in_ratio > max(ratio)):
+            h = img.size[1]
+            w = int(round(h * max(ratio)))
+        else:  # whole image
+            w = img.size[0]
+            h = img.size[1]
+        i = (img.size[1] - h) // 2
+        j = (img.size[0] - w) // 2
+        return i, j, h, w
+
+    def __call__(self, img_group):
+        """
+        Args:
+            img (PIL Image): Image to be cropped and resized.
+        Returns:
+            PIL Image: Randomly cropped and resized image.
+        """
+        i, j, h, w = self.get_params(img_group[0], self.scale, self.ratio)
+        return [F.resized_crop(img, i, j, h, w, self.size, self.interpolation) for img in img_group]
+
+
+class GroupRandomSizedCrop(GroupRandomResizedCrop):
+    """
+    Note: This transform is deprecated in favor of GroupRandomResizedCrop.
+    """
+    def __init__(self, *args, **kwargs):
+        super(GroupRandomSizedCrop, self).__init__(*args, **kwargs)
+
+
+class Stack(object):
+    """ Stack a list of frames into a numpy array"""
+    def __init__(self, ):
+        pass
+
+    def __call__(self, img_group):
+        return np.concatenate([np.expand_dims(x, 0) for x in img_group], axis=0)
+
+
+class ToTorchFormatTensor(object):
+    """ Converts a PIL.Image (RGB) or numpy.ndarray (H x W x C) in the range [0, 255]
+    to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0] """
+    def __init__(self, div=True, Multi_images=False):
+        self.div = div
+        self.Multi_images=Multi_images
+
+    def __call__(self, pic):
+        #print(pic.shape)
+        if isinstance(pic, np.ndarray):
+            # handle numpy array, THWC -> CTHW
+            img = torch.from_numpy(pic.transpose(3, 0, 1, 2))
+        else:
+            # handle PIL Image
+            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+            img = img.view(pic.size[1], pic.size[0], len(pic.mode))
+            # put it from HWC to CHW format
+            # yikes, this transpose takes 80% of the loading time/CPU
+            img = img.transpose(0, 1).transpose(0, 2).contiguous()
+        return img.float().div(255) if self.div else img.float()
+
+
+class IdentityTransform(object):
+
+    def __call__(self, data):
+        return data
+
+
+class GroupOverSample(object):
+    def __init__(self, crop_size, scale_size=None):
+        self.crop_size = crop_size if not isinstance(crop_size, int) else (crop_size, crop_size)
+
+        if scale_size is not None:
+            self.scale_worker = GroupScale(scale_size)
+        else:
+            self.scale_worker = None
+
+    def __call__(self, img_group):
+
+        if self.scale_worker is not None:
+            img_group = self.scale_worker(img_group)
+
+        image_w, image_h = img_group[0].size
+        crop_w, crop_h = self.crop_size
+
+        offsets = GroupMultiScaleCrop.fill_fix_offset(False, image_w, image_h, crop_w, crop_h)
+        oversample_group = list()
+        for o_w, o_h in offsets:
+            normal_group = list()
+            flip_group = list()
+            for i, img in enumerate(img_group):
+                crop = img.crop((o_w, o_h, o_w + crop_w, o_h + crop_h))
+                normal_group.append(crop)
+                flip_crop = crop.copy().transpose(Image.FLIP_LEFT_RIGHT)
+
+                if img.mode == 'L' and i % 2 == 0:
+                    flip_group.append(ImageOps.invert(flip_crop))
+                else:
+                    flip_group.append(flip_crop)
+
+            oversample_group.extend(normal_group)
+            oversample_group.extend(flip_group)
+        return oversample_group
+
+
+class GroupMultiScaleCrop(object):
+
+    def __init__(self, input_size, scales=None, max_distort=1, fix_crop=True, more_fix_crop=True):
+        self.scales = scales if scales is not None else [1, 0.875, .75, .66]
+        self.max_distort = max_distort
+        self.fix_crop = fix_crop  # if False, random
+        self.more_fix_crop = more_fix_crop
+        self.input_size = input_size if not isinstance(input_size, int) else [input_size, input_size]
+        self.interpolation = Image.BILINEAR
+
+    def __call__(self, img_group):
+
+        im_size = img_group[0].size  # this is imgs' size, however, for network, 224 is 2dconvs, 112 is 3dconvs.
+        # in this experiment im_size = 340*256
+        crop_w, crop_h, offset_w, offset_h = self._sample_crop_size(im_size)
+        crop_img_group = [img.crop((offset_w, offset_h, offset_w + crop_w, offset_h + crop_h)) for img in img_group]
+        ret_img_group = [img.resize((self.input_size[0], self.input_size[1]), self.interpolation)
+                         for img in crop_img_group]
+        return ret_img_group
+
+    def _sample_crop_size(self, im_size):
+        image_w, image_h = im_size[0], im_size[1]
+
+        # find a crop size
+        base_size = min(image_w, image_h)
+        crop_sizes = [int(base_size * x) for x in self.scales]
+        crop_h = [self.input_size[1] if abs(x - self.input_size[1]) < 3 else x for x in crop_sizes]
+        crop_w = [self.input_size[0] if abs(x - self.input_size[0]) < 3 else x for x in crop_sizes]
+
+        pairs = []
+        for i, h in enumerate(crop_h):
+            for j, w in enumerate(crop_w):
+                if abs(i - j) <= self.max_distort:
+                    pairs.append((w, h))
+
+        crop_pair = random.choice(pairs)
+        if not self.fix_crop:
+            w_offset = random.randint(0, image_w - crop_pair[0])
+            h_offset = random.randint(0, image_h - crop_pair[1])
+        else:
+            w_offset, h_offset = self._sample_fix_offset(image_w, image_h, crop_pair[0], crop_pair[1])
+            # for 3dconv network, arguments is (340,256,112,112)
+        return crop_pair[0], crop_pair[1], w_offset, h_offset
+
+    def _sample_fix_offset(self, image_w, image_h, crop_w, crop_h):
+        offsets = self.fill_fix_offset(self.more_fix_crop, image_w, image_h, crop_w, crop_h)
+        return random.choice(offsets)
+
+    @staticmethod
+    def fill_fix_offset(more_fix_crop, image_w, image_h, crop_w, crop_h):
+        w_step = (image_w - crop_w) / 4
+        h_step = (image_h - crop_h) / 4
+
+        ret = list()
+        ret.append((0, 0))  # upper left
+        ret.append((4 * w_step, 0))  # upper right
+        ret.append((0, 4 * h_step))  # lower left
+        ret.append((4 * w_step, 4 * h_step))  # lower right
+        ret.append((2 * w_step, 2 * h_step))  # center
+
+        if more_fix_crop:
+            ret.append((0, 2 * h_step))  # center left
+            ret.append((4 * w_step, 2 * h_step))  # center right
+            ret.append((2 * w_step, 4 * h_step))  # lower center
+            ret.append((2 * w_step, 0 * h_step))  # upper center
+
+            ret.append((1 * w_step, 1 * h_step))  # upper left quarter
+            ret.append((3 * w_step, 1 * h_step))  # upper right quarter
+            ret.append((1 * w_step, 3 * h_step))  # lower left quarter
+            ret.append((3 * w_step, 3 * h_step))  # lower righ quarter
+
+        return ret
+
+
+class RandomCropNp(object):
+    """Crop the given video sequences (t x h x w) at a random location.
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    @staticmethod
+    def get_params(img, output_size):
+        """Get parameters for ``crop`` for a random crop.
+        Args:
+            img (PIL Image): Image to be cropped.
+            output_size (tuple): Expected output size of the crop.
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for random crop.
+        """
+        t, h, w, c = img.shape
+        th, tw = output_size
+        if w == tw and h == th:
+            return 0, 0, h, w
+
+        i = random.randint(0, h - th) if h != th else 0
+        j = random.randint(0, w - tw) if w != tw else 0
+        return i, j, th, tw
+
+    def __call__(self, imgs):
+
+        i, j, h, w = self.get_params(imgs, self.size)
+
+        imgs = imgs[:, i:i + h, j:j + w, :]
+        return imgs
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
+
+
+class CenterCropNp(object):
+    """Crops the given seq Images at the center.
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, imgs):
+        """
+        Args:
+            img (PIL Image): Image to be cropped.
+        Returns:
+            PIL Image: Cropped image.
+        """
+        t, h, w, c = imgs.shape
+        th, tw = self.size
+        i = int(np.round((h - th) / 2.))
+        j = int(np.round((w - tw) / 2.))
+
+        return imgs[:, i:i + th, j:j + tw, :]
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
+
+
+class RandomHorizontalFlipNp(object):
+    """Horizontally flip the given seq Images randomly with a given probability.
+    Args:
+        p (float): probability of the image being flipped. Default value is 0.5
+    """
+
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, imgs):
+        """
+        Args:
+            img (seq Images): seq Images to be flipped.
+        Returns:
+            seq Images: Randomly flipped seq images.
+        """
+        if random.random() < self.p:
+            # t x h x w
+            return np.flip(imgs, axis=2).copy()
+        return imgs
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(p={})'.format(self.p)
+
+
+def _test():
+    from torch.utils.data import Dataset, DataLoader
+    import os, datetime
+
+    class VideoRecord(object):
+        def __init__(self, row):
+            self._data = row
+
+        @property
+        def path(self):
+            return self._data[0]
+
+        @property
+        def num_frames(self):
+            return int(self._data[1])
+
+        @property
+        def label(self):
+            return int(self._data[2])
+
+
+    class VerbRgb(Dataset):
+        def __init__(self, is_val=False, transforms=None):
+            self.video_list = [VideoRecord(x.strip().split(' ')) for x in ['/Users/tqvinh/Documents/lib/c3d/data/roger/ 16 0'] * 30]
+            self.transforms = transforms
+            self.is_val = is_val
+
+        def __getitem__(self, index):
+            record = self.video_list[index]
+            num_frames_necessary = 16
+
+            # Get start offset
+            offset = 0
+            if num_frames_necessary < record.num_frames:
+                # If there are more frames, then sample starting offset.
+                diff = (record.num_frames - num_frames_necessary)
+                # temporal augmentation
+                if not self.is_val:
+                    offset = np.random.randint(0, diff)
+
+            frames = [Image.open(os.path.join(record.path, 'frame_%06d.png' % (x + 1))) for x in range(offset, offset + min(num_frames_necessary, record.num_frames))]
+
+            if len(frames) < num_frames_necessary:
+                # Pad last frame if video is shorter than necessary
+                frames.extend([frames[-1]] * (num_frames_necessary - len(frames)))
+
+            frames = self.transforms(frames)
+
+            return frames, record.label
+
+        def __len__(self):
+            return len(self.video_list)
+
+    train_transforms = torchvision.transforms.Compose([
+        GroupResize(128),
+        GroupRandomResizedCrop(size=112),
+        GroupRandomHorizontalFlip(),
+        Stack(),
+        ToTorchFormatTensor(),
+        # GroupNormalize(
+        #     mean=[.5],
+        #     std=[.5])
+    ])
+
+    from torchvision.utils import save_image
+
+    train_dataset = VerbRgb(is_val=False, transforms=train_transforms)
+    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=0, pin_memory=False)
+
+    for i_batch, (inputs, targets) in enumerate(train_loader):
+        
+        print(i_batch, inputs.shape, targets.shape, inputs.min(), inputs.max())
+        data = []
+        for vid in inputs:
+            data += [vid[:, i, :, :] for i in range(vid.shape[1])]
+        data = torch.stack(data)
+
+        save_image(data, './data/temp/vid_%d.jpg' % i_batch, nrow=16)
+
+
+def _test_imgs():
+    from torch.utils.data import Dataset, DataLoader
+    import os, datetime
+
+    class VideoRecord(object):
+        def __init__(self, row):
+            self._data = row
+
+        @property
+        def path(self):
+            return self._data[0]
+
+        @property
+        def num_frames(self):
+            return int(self._data[1])
+
+        @property
+        def label(self):
+            return int(self._data[2])
+
+
+    class VerbRgb(Dataset):
+        def __init__(self, is_val=False, transforms=None):
+            self.video_list = [VideoRecord(x.strip().split(' ')) for x in ['/Users/tqvinh/Documents/lib/c3d/data/roger/frame_000001.png 16 0'] * 32]
+            self.transforms = transforms
+            self.is_val = is_val
+
+        def __getitem__(self, index):
+            record = self.video_list[index]
+            frame = Image.open(record.path)
+            frame = self.transforms(frame)
+            return frame, record.label
+
+        def __len__(self):
+            return len(self.video_list)
+
+    train_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.RandomResizedCrop(size=224, scale=(0.1, 1.2)),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor()
+    ])
+
+    from torchvision.utils import save_image
+
+    train_dataset = VerbRgb(is_val=False, transforms=train_transforms)
+    train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True, num_workers=0, pin_memory=False)
+
+    for i_batch, (inputs, targets) in enumerate(train_loader):
+        print(i_batch, inputs.shape, targets.shape, inputs.min(), inputs.max())
+        save_image(inputs, './data/temp/img_%d.jpg' % i_batch, nrow=8)
+
+
+if __name__ == "__main__":
+    _test()
